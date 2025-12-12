28bd21e6eb66d823718ce10803364b8ff84fc8a4
MilesPope <mp1037@exeter.ac.uk>
2025-12-12 16:16:30 +0000

    Staging for Stage 3: AI intergration

---
+++ Stage 3/components.py
@@ -0,0 +1,125 @@
+"""
+Module providing initialisation functions for board and methods to print and check legal moves
+"""
+
+from colorama import Fore
+
+def initialise_board(size:int = 8) -> list:
+    """
+    Return an initialised Othello board for the given size
+    
+    :param size: Board dimension (default 8)
+    :type size: int
+    :return: 2D list representing starting board
+    :rtype: list
+    """
+
+    # Board size must be even from starting 2x2 tokens
+    if size % 2 != 0:
+        raise ValueError("Board size must be an even number")
+    # Board size must be positive and greater than two
+    if size < 2:
+        raise ValueError("Board size must be greater than two")
+    # Initialise the board:
+    board = []
+
+    # Fill in board
+    for _ in range(size):
+        board.append([None] * size)
+
+    # Place starting tokens
+    mid = size // 2
+
+    board[mid-1][mid-1] = "Light"
+    board[mid-1][mid] = "Dark "
+    board[mid][mid-1] = "Dark "
+    board[mid][mid] = "Light"
+
+    # Return the initialised board
+    return board
+
+def print_board(board:list) -> None:
+    """
+    Print an ASCII representation of the board state to the command line
+    
+    :param board: variable dimension list
+    :type board: list
+    """
+
+    representation_map = {
+        "Light" : "W",
+        "Dark " : "B",
+        None : "N"
+    }
+    colour_map = {
+        "Light" : Fore.GREEN,
+        "Dark " : Fore.RED,
+        None: Fore.WHITE
+    }
+
+    for row in board:
+        for cell in row:
+            try:
+                cell_representation = representation_map[cell]
+                colour_representation = colour_map[cell]
+            except KeyError:
+                raise KeyError(f"Unrecognised board entry {cell}") from None
+            print(colour_representation + "|" + cell_representation + "|", end="")
+        print("\n")
+
+def legal_move(colour:str, coord:tuple, board:list) -> bool:
+    """
+    Check whether a given move of placing a counter at a coordinate as a player is a legal move
+    
+    :param colour: either "Dark " or "Light" depending on who's turn it is
+    :type colour: str
+    :param coord: coordinate to check
+    :type coord: tuple
+    :param board: given board
+    :type board: list
+    :return: boolean statement saying whether move is legal or not.
+    :rtype: bool
+    """
+
+    # Initialise variables
+    x = coord[0]
+    y = coord[1]
+    opponent = "Dark " if colour == "Light" else "Light"
+    board_size = len(board)
+
+    # Check whether coordinate is empty
+    try:
+        if board[y][x] is not None:
+            return False
+    except IndexError:
+        raise IndexError from None
+
+    # Check whether coordinate outflanks at least once peice
+    # Go through each direction to check there is at least one line containing at,
+    # least one opposing peice followed by one player peice
+    for dx in [-1,0,1]:
+        for dy in [-1,0,1]:
+            next_opponent = False
+            terminating_player = False
+            neighbour_x = x + dx
+            neighbour_y = y + dy
+            if 0 <= neighbour_x < board_size and 0 <= neighbour_y < board_size:
+                # If the cell is of opponent colour:
+                if board[neighbour_y][neighbour_x] == opponent:
+                    next_opponent = True # The peice adjacent belongs to opponent
+                    next_x = neighbour_x
+                    next_y = neighbour_y
+                    while 0 <= next_x+dx < board_size and 0 <= next_y+dy < board_size:
+                        # Increment the next x and y
+                        next_x += dx
+                        next_y += dy
+                        # Check if the next cell belongs to the player
+                        if board[next_y][next_x] == colour:
+                            terminating_player = True
+            if terminating_player and next_opponent:
+                # print(f"Found line which outflanks dx={dx}, dy={dy}")
+                return True
+    return False
+
+cur_board = initialise_board()
+print_board(cur_board)
---
+++ Stage 3/game_engine.py
@@ -0,0 +1,198 @@
+"""
+Module containing main game loop
+"""
+
+from components import initialise_board, legal_move, print_board
+
+def cli_coords_input() -> tuple:
+    """
+    Get an input for coordinates from the client
+    
+    :return: inputted coordinates if they are valid
+    :rtype: tuple
+    """
+    valid_input = False
+    while not valid_input:
+        try:
+            x_coord = int(input("Enter x coordinate: "))
+            y_coord = int(input("Enter y coordinate: "))
+            if x_coord < 0 or y_coord < 0:
+                print("Coordinates must be positive")
+            else:
+                valid_input = True
+        except TypeError:
+            print("Enter an integer")
+        except ValueError:
+            print("Do not enter a blank string")
+    # print("abc")
+    return (x_coord,y_coord)
+
+def outflanked(board:list, colour:str, coords:list) -> list:
+    """
+    Change outflanked tokens to the player's colour
+    
+    :param board: current board
+    :param colour: colour of player who placed outflanking token
+    :param coords: coordinates of placed token
+    :return: return the updated board
+    :rtype: list
+    """
+    size = len(board)
+    for dx in [-1,0,1]:
+        for dy in [-1,0,1]:
+
+            # Skip if no dx or dy
+            if dx == dy and dx == 0:
+                continue
+
+            cur_coords = [coords[0] + dx, coords[1] + dy]
+            line_coords = []
+            flip_line = False
+
+            while 0 <= cur_coords[0] < size and 0 <= cur_coords[1] < size:
+                cur_token = board[cur_coords[1]][cur_coords[0]]
+                if cur_token is None:
+                    break
+
+                elif cur_token == colour:
+                    flip_line = True
+                    break
+                
+                line_coords.append(cur_coords.copy()) # prevent just same thing being appended
+                cur_coords[0] += dx
+                cur_coords[1] += dy
+
+            if flip_line and line_coords:
+                for x, y in line_coords:
+                    board[y][x] = colour
+
+    return board
+
+def has_legal_move(board, colour) -> bool:
+    """
+    Check if there is a possible move for a given player
+    
+    :param board: 2D list representing board
+    :param colour: string representing the player
+    :return: boolean representing if the player has a possible move
+    :rtype: bool
+    """
+    for x in range(len(board)):
+        for y in range(len(board)):
+            if board[x][y] is not None:
+                pass
+            elif legal_move(colour, (x,y), board):
+                return True
+    return False
+
+def check_win(board:list) -> list:
+    """
+    Given a board, return the amount of counters each player has and who has won
+    
+    :param board: 2d array representing the board
+    :type board: list
+    :return: a list containing a tuple with scores (Light, black) and a winner
+    :rtype: list
+    """
+    light_tokens, black_tokens = 0, 0
+    for row in board:
+        for cell in row:
+            if cell == "Dark ":
+                black_tokens += 1
+            elif cell == "Light":
+                light_tokens += 1
+
+    winner = None
+    if black_tokens > light_tokens:
+        winner = "Dark "
+    elif light_tokens > black_tokens:
+        winner = "Light"
+    else:
+        winner = "Draw"
+
+    return ((light_tokens, black_tokens), winner)
+
+class GameState:
+    """
+    Class for storing game state details and utility for transfering to and from JSON
+    (with typing because I miss java)
+    """
+    def __init__(self, board:list, cur_player:str, finished:bool=False) -> None:
+        self.board = board
+        self.cur_player = cur_player
+        self.finished = finished
+
+    def to_dict(self) -> dict:
+        """
+        Return instance information as a dictionary
+        """
+        return {
+            "board" : self.board,
+            "cur_player" : self.cur_player,
+            "finished" : self.finished
+        }
+
+    @classmethod # Not to do with the instance: to do with the class.
+    def from_dict(cls: type["GameState"], data:dict) -> "GameState":
+        """
+        Initialise GameState class from dictionary data to allow for loading JSON saves
+        
+        :param cls: class for data to be loaded into
+        :param data: dictionary
+        """
+        return cls(data["board"], data["cur_player"], data["finished"])
+
+def simple_game_loop() -> None:
+    """
+    Simple game loop for intermediate manual testing through CLI
+    """
+    # Start the game with a welcome message:
+    print( "#" * 27 + "\n" + "#" + " Welcome to Othello game " +"#" + "\n"+ "#" * 27 )
+
+    # Initialise variables for game
+    cur_board = initialise_board()
+    move_counter = 60
+    while move_counter > 0:
+        # Check which players have possible moves
+        black_has_legal = has_legal_move(cur_board, "Dark ")
+        light_has_legal = has_legal_move(cur_board, "Light")
+        # If neither player has possible turns, quit the loop:
+        if not (black_has_legal or light_has_legal):
+            break
+        # By default, Dark goes first:
+        if move_counter % 2 == 0 and black_has_legal:
+            cur_player = "Dark "
+        else:
+            cur_player = "Light"
+
+        # Display info to CLI
+        print_board(cur_board)
+        print(f"{move_counter} turns left\n{cur_player} is up")
+        for x1 in range(8):
+            for y1 in range(8):
+                if legal_move(cur_player, (x1,y1), cur_board):
+                    print(f"({y1}, {x1}) legal for {cur_player}")
+
+        # Get current player to make their move:
+        move_made = False
+        while not move_made:
+            move_coords = cli_coords_input()
+            move_possible = legal_move(cur_player, move_coords, cur_board)
+            if move_possible:
+                print("Move is possible")
+                cur_board[move_coords[1]][move_coords[0]] = cur_player
+                cur_board = outflanked(cur_board, cur_player, move_coords)
+                move_made = True
+            else:
+                print("Invalid move")
+        move_counter -= 1
+
+    # Game is over
+    check_winner = check_win(cur_board)
+    if check_winner[1] != "Draw":
+        print(f"{check_winner[1]} has won {check_winner[0][0]}:{check_winner[0][1]}")
+    else:
+        print(f"Draw at {check_winner[0][0]}!")
+
+if __name__ == "__main__":
+    simple_game_loop()
